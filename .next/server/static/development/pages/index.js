dcecae01f265e7dad433c53e4c62e1e8b430360b
Daniel Schmitz <danieljfa@gmail.com>
2021-03-22 12:48:15 +0000

    First Commit

--- .next/server/static/development/pages/index.js
+++ .next/server/static/development/pages/index.js
@@ -104,7 +104,7 @@ module.exports =
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-var _jsxFileName = "/home/runner/FortunateTallCodec/pages/index.js";
+var _jsxFileName = "/home/runner/NextFromReplit/pages/index.js";
 
 var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
 
@@ -129,7 +129,7 @@ function HomePage() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/runner/FortunateTallCodec/pages/index.js */"./pages/index.js");
+module.exports = __webpack_require__(/*! /home/runner/NextFromReplit/pages/index.js */"./pages/index.js");
 
 
 /***/ }),
